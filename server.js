9980233a5ce578306100bcff7bf23cfd742566b5
AmirV18 <amirvassell1@gmail.com>
2022-10-15 00:21:03 -0400

    changed server.js heading

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*WEB322 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
+
 * 
 *  Name: ____Amir Vassell_____ Student ID: 154737209 Date: ___October 3, 2022___
 *
